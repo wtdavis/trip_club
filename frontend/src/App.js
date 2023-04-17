c85e415c925c0c255f3e359567f8239822abaf25
Olga <olga.sleepless@gmail.com>
2023-04-17 17:32:17 -0400

    Update Trip component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,8 +9,11 @@ import SplashPage from './components/SplashPage/SplashPage';
 import LoginForm from './components/SessionForms/LoginForm';
 import SignupForm from './components/SessionForms/SignupForm';
 import Tweets from './components/Tweets/Tweets';
+// import Trips from './components/Trips/Trips';
 import Profile from './components/Profile/Profile';
 import TweetCompose from './components/Tweets/TweetCompose';
+// import TripShow from './components/Trips/TripShow';
+// import TripForm from './components/TripForm/TripForm';
 
 import { getCurrentUser } from './store/session';
 
@@ -30,8 +33,11 @@ function App() {
         <AuthRoute exact path="/signup" component={SignupForm} />
 
         <ProtectedRoute exact path="/tweets" component={Tweets} />
+        {/* <ProtectedRoute exact path="/trips" component={Trips} /> */}
         <ProtectedRoute exact path="/profile" component={Profile} />
         <ProtectedRoute exact path="/tweets/new" component={TweetCompose} />
+        {/* <ProtectedRoute exact path="/trips/new" component={TripForm} /> */}
+        {/* <ProtectedRoute exact path="/trips/show" component={TripShow} /> */}
       </Switch>
     </>
   );
