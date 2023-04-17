91024fa35ff3e50ad19d1d7be74739d1930025f0
Olga <olga.sleepless@gmail.com>
2023-04-17 11:25:32 -0400

    Add frontend auth

---
+++ frontend/src/components/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+function MainPage() {
+  return (
+    <>
+      <p>A Twitter Clone</p>
+      <footer>
+        Copyright &copy; 2022 MERN Twitter
+      </footer>
+    </>
+  );
+}
+
+export default MainPage;
