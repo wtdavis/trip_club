658f18c6145bc75e142ad6183bdb7fcfd65b6137
Olga <olga.sleepless@gmail.com>
2023-04-18 16:22:16 -0400

    Update ProfilePage and trip store

--- frontend/src/components/Profile/ProfilePage.js
+++ frontend/src/components/Profile/ProfilePage.js
@@ -10,12 +10,10 @@ import GoogleMap from '../GoogleMap'
 
 const ProfilePage = () => {
   const dispatch = useDispatch();
-  // debugger;
   const currentUser = useSelector(state => state.session.user);
-  // debugger
-  const trips = useSelector(tripActions.getTrips); 
-  console.log(trips);
-  // const tripsSelected = state => Object.values(state.trips))
+  // const trips = useSelector(tripActions.getTrips); 
+  const trips = useSelector(state => Object.values(state.trips.all))
+  const tripsUser = trips.filter(trip => trip.author._id == currentUser._id);
 
   
   useEffect(() => {
@@ -31,11 +29,8 @@ const ProfilePage = () => {
   let lng = -73.99376925185645;
   let lat = 40.73631643149453;
 
-  // if (trips){
-  //   const tripsSelected = trips[0].filter(trip => trip.author._id == currentUser._id);
-  // }
 if (trips) {
-  if (!trips) {
+  if (trips.length == 0) {
     return (
      <>     
       <div>{currentUser.username} has no Trips</div>
@@ -49,20 +44,22 @@ if (trips) {
       <>
         <h2>All of {currentUser.username}'s Trips</h2>
         <div>
-        <GoogleMap lng={lng} lat={lat}/>
-      </div>
+          <GoogleMap lng={lng} lat={lat}/>
+        </div>
         {/* {tripsSelected.map(trip => (
           <TripItem
             key={trip._id}
             trip={trip}
           />
         ))} */}
-        {/* {trips[0].map(trip => (
-          <TripTest 
+        {tripsUser ? tripsUser.map(trip => (
+          <TripItem 
             key={trip._id}
             trip={trip} 
           />
-        ))} */}
+        )) : 
+        <></>
+        }
 
       </>
     );
