f050c315348a69f007123f4e1ee4b3ea843c9e3d
Olga <olga.sleepless@gmail.com>
2023-04-21 06:07:26 -0400

    Add event styles

--- frontend/src/components/EventForm/EventForm.js
+++ frontend/src/components/EventForm/EventForm.js
@@ -29,23 +29,58 @@ function EventForm (props) {
     }
 return(
     <div className="eventformdiv">
-        <h3 className="eventformheader">Create a New Event!</h3>
+        <header className="createevent_header">
+          <div className="eventformheader">Create a New Event!</div>
+        </header> 
+        {/* <h3 className="eventformheader">Create a New Event!</h3> */}
         <form classname="eventformform" onSubmit={e => handleSubmit(e)}>
        
-            <p className="eventformsubheader">Name Your New Event:</p>
-            <input type="text" className="eventforminput" value={title} onChange={e => setTitle(e.target.value)}/>
+            {/* <p className="eventformsubheader">Name Your New Event:</p> */}
+            <input 
+                className="createevent_input" 
+                type="text" 
+                value={title} 
+                onChange={e => setTitle(e.target.value)}
+                placeholder="Name Your New Event"
+            />
             
-            <p className="eventformsubheader">Enter a description:</p>
-            <textarea className="eventforminput" value={description} onChange={e => setDescription(e.target.value)}/>
+            {/* <p className="eventformsubheader">Enter a description:</p> */}
+            <textarea 
+                className="createevent_input description"
+                value={description}
+                onChange={e => setDescription(e.target.value)}
+                placeholder="Enter a description"
+            />
             
             <p className="eventformsubheader">Event Start Date:</p>
-            <input type="date" className="eventforminput" value={startDate} onChange={e => setStartDate(e.target.value)}/>
+            <input 
+                className="createevent_input" 
+                type="date" 
+                value={startDate} 
+                onChange={e => setStartDate(e.target.value)}
+            />
             
             <p className="eventformsubheader">Event End Date:</p>
-            <input type="date" className="eventforminput" value={endDate} onChange={e => setEndDate(e.target.value)}/>
+            <input 
+                type="date" 
+                className="createevent_input" 
+                value={endDate} 
+                onChange={e => setEndDate(e.target.value)}
+            />
             {/* <Calendar/> */}
             <br/>
-            <input type="submit" className="eventformsubmit"  value="Create!" onClick={e=> handleSubmit(e)}/>
+            <button 
+                className="eventformsubmit" 
+                type="submit" 
+                // value={submit} 
+                onClick={e=> handleSubmit(e)}
+            >
+                Continue
+            </button>
+            {/* <button type="submit" className="tripcreate_button" value={submit} onClick={e=> handleSubmit(e)}>Continue</button> */}
+
+
+            {/* <input type="submit" className="eventformsubmit"  value="Create!" onClick={e=> handleSubmit(e)}/> */}
         </form>
     </div>
 )
