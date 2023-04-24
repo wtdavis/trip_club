a234f41661639ea7a66c4570c74be40d9d368025
wtdavis <wtdavis@wesleyan.edu>
2023-04-24 13:46:34 -0400

    calendar error commented out

--- frontend/src/components/EventForm/EventForm.js
+++ frontend/src/components/EventForm/EventForm.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import Calendar from "react-calendar";
+// import Calendar from "react-calendar";
 import { createTripEvent } from "../../store/events";
 import "./EventForm.css"
 
