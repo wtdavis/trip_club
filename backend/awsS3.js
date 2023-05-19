753e82f70dcf03a822e1f0102f1f2473417500db
wtdavis <wtdavis@wesleyan.edu>
2023-05-19 15:08:18 -0400

    fix trip create bug

--- backend/awsS3.js
+++ backend/awsS3.js
@@ -23,11 +23,12 @@ const singleFileUpload = async ({ file, public = false }) => {
 };
 
 const multipleFilesUpload = async ({files, public = false}) => {
+  if (files){
   return await Promise.all(
     files.map((file) => {
       return singleFileUpload({file, public});
     })
-  );
+  )};
 };
 
 const retrievePrivateFile = (key) => {
