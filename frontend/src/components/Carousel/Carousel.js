1d3648e4fc74027210d7d6ce0b237e03ee8bf08e
Olga <olga.sleepless@gmail.com>
2023-05-22 21:45:25 -0400

    Update contact us page

--- frontend/src/components/Carousel/Carousel.js
+++ frontend/src/components/Carousel/Carousel.js
@@ -11,23 +11,11 @@ function Carousel(tripImages) {
   let slides = [];
   
   if (images.length === 0) {
-    slides = [
-      {url: "https://trip-club-dev.s3.amazonaws.com/trips/1/event1/1.jpg"},
-      {url: "https://trip-club-dev.s3.amazonaws.com/trips/1/event1/2.jpg"},
-      {url: "https://trip-club-dev.s3.amazonaws.com/trips/1/event1/3.jpg"},
-      {url: "https://trip-club-dev.s3.amazonaws.com/trips/1/event3/1.jpg"}         
-    ];
+    slides.push({url: "https://trip-club-dev.s3.amazonaws.com/trips/default_trip.jpg"});
   } else {
     for (let i = 0; i < images.length; i++) {
       slides.push({url: images[i]})
     }
-    // slides = [
-    //   {url: images[0]},
-    //   {url: images[1]},
-    //   {url: images[2]},
-    //   {url: images[3]},
-    //   {url: images[4]}
-    // ]
   }
 
   const prevPhoto = () => {
