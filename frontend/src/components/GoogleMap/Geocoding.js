0cfa91f31ddf9a5f4c26fa0227e057b3e9c0cc0e
Olga <olga.sleepless@gmail.com>
2023-06-01 17:25:10 -0400

    Update Geocoding

--- frontend/src/components/GoogleMap/Geocoding.js
+++ frontend/src/components/GoogleMap/Geocoding.js
@@ -5,15 +5,14 @@ const Geocoding = () => {
   //
   const [geocodeData, setGeocodeData] = useState(null);
   const [address, setAddress] = useState('1600 Amphitheatre Parkway, Mountain View, CA');
+  const apiKey = process.env.REACT_APP_MAPS_API_KEY; 
+  // let address = '22 Main st Boston MA';
+  // let address = '1600 Amphitheatre Parkway, Mountain View, CA'
 
   
   useEffect(() => {
     const fetchData = async() => {
       try{
-        // let address = '22 Main st Boston MA';
-        // let address = '1600 Amphitheatre Parkway, Mountain View, CA'
-        const apiKey = process.env.REACT_APP_MAPS_API_KEY; 
-        // setAddress('Boston MA');
         // debugger
         
         let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`;
@@ -24,15 +23,15 @@ const Geocoding = () => {
           const data = await response.json();
           setGeocodeData(data);
         } else {
-          console.error('Request failed with status code: ', response.status);
+          console.error('Unsuccessful request with status code: ', response.status);
         }
       } catch (error) {
-        console.error('Request failed: ', error);
+        console.error('Unsuccessful request with error: ', error);
       }
     };
 
     fetchData();
-  }, []);
+  }, [address]);
 
   if (!geocodeData) {
     return <div>Loading...</div>;
@@ -45,16 +44,43 @@ const Geocoding = () => {
     const lat = results[0].geometry.location.lat;
     const lng = results[0].geometry.location.lng;
 
+    const handleUpdateAddress = async () => {
+      try {
+        let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`;
+        const response = await fetch(url);
+
+        debugger
+
+        if (response.ok) {
+          const data = await response.json();
+          setGeocodeData(data);
+        } else {
+          console.error('Unsuccessful request with status code: ', response.status);
+        }
+      } catch (error) {
+        console.error('Unsuccessful request with error: ', error);
+      }
+    };
+
     return (
       <div>
         <input
           className="trip_address_input"
           type='text'
           value={address}
-          // onChange={e => {setAddress(e.target.value)}}
-          placeholder="Address"
+          onChange={e => {setAddress(e.target.value)}}
+          placeholder="Address"          
         />
+
+        <button 
+          className="addaddress_button"
+          onClick={handleUpdateAddress}
+        >
+          Update Address
+        </button>
         <p>Latitude: {lat}, Longitude: {lng}</p>
+
+
       </div>
     )}
 }
